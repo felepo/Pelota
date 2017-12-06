f22c4ad9dbbf7f51dee1c7218692a7c2f9cbf46c
Fernando Poncio <fecrackle@gmail.com>
2017-12-05 23:29:49 -0600

    Estableciendo Records y Dificultades

--- app/src/main/java/com/felepo/pelota/Gestion.java
+++ app/src/main/java/com/felepo/pelota/Gestion.java
@@ -1,5 +1,6 @@
 package com.felepo.pelota;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ public class Gestion extends AppCompatActivity
     private int dificultad;
     private int FPS = 30;
     private Handler temporizador;
+    private int toques;             //Variable para guardar el record de toques
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,11 +20,15 @@ public class Gestion extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gestion);
 
+        toques = 0;
+
         Bundle extras = getIntent().getExtras();
         dificultad = extras.getInt("DIFICULTAD");
 
         partida = new Partida(getApplicationContext(), dificultad);
         setContentView(partida);
+
+        //Esto sirve para especificar en cuanto tiempo la pelota empieza a caer
         temporizador = new Handler();
         temporizador.postDelayed(elHilo, 2000);
     }
@@ -49,7 +55,11 @@ public class Gestion extends AppCompatActivity
         int x = (int) evento.getX();
         int y = (int) evento.getY();
 
-        partida.toque(x, y);
+        //Si el toque es correcto, entonces se aumenta la variable
+        if( partida.toque(x, y) )
+        {
+            toques++;
+        }
 
         return false;
     }
@@ -58,6 +68,12 @@ public class Gestion extends AppCompatActivity
     {
         //Limpia el hilo que se usó
         temporizador.removeCallbacks(elHilo);
+
+        //Antes de destruir el activity, se manda el record con el bundle
+        Intent in = new Intent();
+        in.putExtra("PUNTUACION", toques);
+
+        setResult(RESULT_OK, in);
 
         //Destruye el activity actual
         finish();
--- app/src/main/java/com/felepo/pelota/MainActivity.java
+++ app/src/main/java/com/felepo/pelota/MainActivity.java
@@ -1,15 +1,23 @@
 package com.felepo.pelota;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    int record;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
@@ -26,12 +34,68 @@ public class MainActivity extends AppCompatActivity {
 
         int dificultad = 1;
 
-        if( dific.equals("Standard") ) dificultad = 2;
-        if( dific.equals("Dificult") ) dificultad = 3;
+        if( dific.equals(getString(R.string.medio)) ) dificultad = 2;
+        if( dific.equals(getString(R.string.dificil)) ) dificultad = 3;
 
         Intent in = new Intent(this, Gestion.class);
         in.putExtra("DIFICULTAD", dificultad);
 
-        startActivity(in);
+        //startActivity(in);
+        startActivityForResult(in, 1);
+    }
+
+    //Todo esto se ejecuta cuando se termina de jugar
+    protected void onActivityResult(int peticion, int codigo, Intent in)
+    {
+        if( (peticion != 1) || (codigo != RESULT_OK) )
+        {
+            return;
+        }
+
+        int resultado = in.getIntExtra("PUNTUACION", 0);
+
+        //Se verifica si se ha roto el record anterior
+        if( resultado > record )
+        {
+            record = resultado;
+
+            TextView caja = (TextView) findViewById(R.id.record);
+            caja.setText("Record: " + record);
+
+            guardarRecord();
+        }
+        else
+        {
+            String puntuacionPartida = " " + resultado;
+
+            Toast miToast = Toast.makeText(this, puntuacionPartida, Toast.LENGTH_SHORT);
+            miToast.setGravity(Gravity.CENTER, 0, 0);
+            miToast.show();
+        }
+    }
+
+    public void onResume()
+    {
+        super.onResume();
+
+        leerRecord();
+    }
+
+    private void guardarRecord()
+    {
+        SharedPreferences datos = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor miEditor = datos.edit();
+        miEditor.putInt("RECORD", record);
+
+        miEditor.apply();
+    }
+
+    private void leerRecord()
+    {
+        SharedPreferences datos = PreferenceManager.getDefaultSharedPreferences(this);
+        record = datos.getInt("RECORD", 0);
+
+        TextView caja = (TextView) findViewById(R.id.record);
+        caja.setText("Record: " + record);
     }
 }
